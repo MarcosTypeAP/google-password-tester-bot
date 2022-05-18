d5afbbef3141872e71e32fe322c070f6315d4272
MarcosTypeAP <marcosarielpaccor@gmail.com>
2022-05-18 13:30:04 -0300

    Small refactoring

--- gen-password-combinations.py
+++ gen-password-combinations.py
@@ -1,30 +1,32 @@
+# Python
 import itertools
 import sys
 
 
-def get_passwd_parts_groups(file_name):
-    passwd_parts_groups = []
+def get_passwd_part_groups(file_name):
+    passwd_part_groups = []
 
     with open(file_name, 'r') as fp:
-        current_part = 0
+        current_part = 1
         line_num = 0
 
-        for fl in fp.readlines():
+        for file_line in fp.readlines():
             line_num += 1
+            file_name = file_name.strip()
 
-            if fl == '\n':
-                double_new_line = len(passwd_parts_groups) != current_part + 1
-                if not passwd_parts_groups or double_new_line:
+            if file_line == '\n':
+                double_new_line = len(passwd_part_groups) != current_part
+                if not passwd_part_groups or double_new_line:
                     print(f'Syntax error in line {line_num}.')
                     return
                 current_part += 1
             else:
-                if len(passwd_parts_groups) == current_part + 1:
-                    passwd_parts_groups[current_part].append(fl[:-1])
+                if len(passwd_part_groups) == current_part:
+                    passwd_part_groups[current_part - 1].append(file_line[:-1])
                 else:
-                    passwd_parts_groups.append([fl[:-1]])
+                    passwd_part_groups.append([file_line[:-1]])
 
-    return passwd_parts_groups
+    return passwd_part_groups
 
 
 def main():
@@ -33,22 +35,23 @@ def main():
     tested_passwords_files = sys.argv[1:]
     tested_passwds = set()
 
-    passwd_parts_groups = get_passwd_parts_groups(input_file)
-    passwds_in_parts = itertools.product(*passwd_parts_groups)
+    passwd_part_groups = get_passwd_part_groups(input_file)
+    if not passwd_part_groups:
+        return
 
-    if tested_passwords_files:
-        for tested_passwords_file in tested_passwords_files:
-            with open(tested_passwords_file, 'r') as fp:
-                tested_passwds.update([
-                    line.rsplit(' ', 1)[0]
-                    for line in fp.readlines()
-                ])
-    #  print(len(list(passwds_in_parts)), len(tested_passwds))
+    passwd_in_parts_list = itertools.product(*passwd_part_groups)
+
+    for tested_passwords_file in tested_passwords_files:
+        with open(tested_passwords_file, 'r') as fp:
+            tested_passwds.update([
+                line.rsplit(' ', 1)[0]  # 'password 0\n'
+                for line in fp.readlines()
+            ])
 
     passwords = (
-        ''.join(passwd_parts) + ' 0\n'
-        for passwd_parts in passwds_in_parts
-        if not tested_passwds or ''.join(passwd_parts) not in tested_passwds
+        ''.join(passwd_in_parts) + ' 0\n'
+        for passwd_in_parts in passwd_in_parts_list
+        if not tested_passwds or ''.join(passwd_in_parts) not in tested_passwds
     )
 
     with open(output_file, 'w') as fp:
--- get-untested-passwords.py
+++ get-untested-passwords.py
@@ -1,14 +1,19 @@
+# Python
+import sys
+
+
 def get_passwd_number(line):
-    return int(line[:-1].rsplit(' ', 1)[-1])
+    # 01/24 19:11:50 |  'password' isn't valid - tested: 32\n
+    return int(line[:-1].rsplit(' ', 1)[1])
 
 
 def main():
+    logs_file = sys.argv[1]
+    passwords_file = sys.argv[2]
     tested_passwd_numbers = set()
     untested_passwd_numbers = []
     new_passwd_lines = []
-
-    with open('logs.txt', 'r') as fp:
-        file_lines = fp.readlines()
+    file_lines = open(logs_file, 'r').readlines()
 
     for line in file_lines:
         if "isn't valid" in line:
@@ -16,9 +21,8 @@ def main():
     tested_passwd_numbers = list(tested_passwd_numbers)
     tested_passwd_numbers.sort()
 
-    tested_passwd_numbers_length = len(tested_passwd_numbers)
     for index, num in enumerate(tested_passwd_numbers):
-        if tested_passwd_numbers_length != index + 1:
+        if len(tested_passwd_numbers) != index + 1:
             if tested_passwd_numbers[index + 1] != num + 1:
                 passwd_numbers = list(range(
                     num + 1,
@@ -26,22 +30,18 @@ def main():
                 ))
                 untested_passwd_numbers += passwd_numbers
 
-    with open('passwords.txt', 'r') as fp:
+    with open(passwords_file, 'r') as fp:
         file_lines = fp.readlines()
 
     passwd_num = 0
     for line in file_lines:
         passwd_num += 1
         if passwd_num in untested_passwd_numbers:
-            passwd = line.split(' ', 1)[0]
-            passwd_line = f'{passwd} 0\n'
+            passwd_line = line.replace('1\n', '0\n')  # 'password 1\n'
             new_passwd_lines.append(passwd_line)
 
     with open('untested-passwords.txt', 'w') as fp:
         fp.writelines(new_passwd_lines)
-
-    for paswd_line in new_passwd_lines:
-        print(passwd_line)
 
 
 if __name__ == '__main__':
